d89625f35e1f3c816cddf55dbb6495cc8ed49fa3
Jkr2255 <magnesium.oxide.play@gmail.com>
2016-04-23 16:57:30 +0900

    license changed to Ruby's/BSDL

--- lib/integer_set.rb
+++ lib/integer_set.rb
@@ -1,3 +1,18 @@
+# Based on set.rb bundled with Ruby.
+#
+# set.rb license:
+#++
+# Copyright (c) 2002-2013 Akinori MUSHA <knu@iDaemons.org>
+#
+# Documentation by Akinori MUSHA and Gavin Sinclair.
+#
+# All rights reserved.  You can redistribute and/or modify it under the same
+# terms as Ruby.
+#
+# for integer_set.rb:
+# Copyright (c) 2016 Jkr2255.
+
+
 require "integer_set/version"
 
 module IntegerSet
